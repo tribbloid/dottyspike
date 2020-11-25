894ab2102b964eb2a3222eb99e251c46af4e3d4d
tribbloid <pc175@uow.edu.au>
2020-11-25 15:41:30 -0500

    add support for vscode

--- src/main/scala/com/tribbloids/spike/meta/staging/Main.scala
+++ src/main/scala/com/tribbloids/spike/meta/staging/Main.scala
@@ -10,6 +10,8 @@ object Main {
   given Toolbox = Toolbox.make(getClass.getClassLoader)
 
   def main(args: Array[String]): Unit = {
+    val self = stagedPower(1)
+    
     val square = stagedPower(2)
     println("3^2 = " + square(3))
     println("4.1^2 = " + square(4.1))
@@ -21,7 +23,7 @@ object Main {
     val toTheFourth = stagedPower(4)
     println("3^4 = " + toTheFourth(3))
     println()
-    
+
     val toThe5 = stagedPower(5)
     println("3^5 = " + toThe5(3))
     println()
@@ -29,21 +31,22 @@ object Main {
 
   def stagedPower(n: Int): Double => Double = {
     // Code representing the labmda where the recursion is unrolled based on the value of n
-    def code(using QuoteContext) = '{ (x: Double) => ${ powerCode(n, 'x) } }
+    def code(using QuoteContext): Expr[Double => Double] = '{ (x: Double) => ${ powerCode(n, 'x) } }
 
     println(s"staged power for n=" + n + ":")
-    
-    println(withQuoteContext{
-      val _code = code
-      _code.show
-    })
 
-    // Evaluate the contents of the code and return it's value
-    run(code)
+    val _code = withQuoteContext{
+      val _code: Expr[Double => Double] = code
+      println(_code.show)
+      _code
+    }
+    
+    // Evaluate the contents of the co/*  */de and return it's value
+    run(_code)
   }
 
-  def powerCode(n: Int, x: Expr[Double])(using ctx: QuoteContext): Expr[Double] =
-    if (n == 0) Expr(1.0) // Expr() lifts 1.0 to '{1.0}
+  def powerCode(n: Int, x: Expr[Double])(using ctx: QuoteContext): Expr[Double] = {
+    val result = if (n == 0) Expr(1.0) // Expr() lifts 1.0 to '{1.0}
     else if (n == 1) x // optimization to not generate x * 1
     else if (n < 0) throw new Exception("Negative powers not implemented. Left as a small exercise. Dont be shy, try it out.")
     else if (n == 2) {
@@ -51,8 +54,11 @@ object Main {
     } // optimization to not generate { val y = x; y * y }
     else if (n % 2 == 1) {
       '{ $x * ${ powerCode(n - 1, x) } }
-    } 
+    }
     else {
       '{ val y = $x * $x; ${ powerCode(n / 2, 'y) } }
     }
+
+    result
+  }
 }
