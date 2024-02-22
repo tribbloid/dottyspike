a5af7a32cd583d23538b0cb70024de6ffc0382e8
peng <peng.cheng@acyclic.ai>
2024-02-21 22:31:02 -0500

    improve AutoLift impl and add a test
    still get :
              java.io.NotSerializableException: scala.quoted.runtime.impl.TypeImpl
    
    but can be fixed easily by the compiler team
    
    add shell scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ allprojects {
 
                 additionalParameters.addAll(
                     listOf(
+//                        "-experimental",
                         "-language:experimental.dependent"
                     )
                 )
