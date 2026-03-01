5d658bca38744d546dabcd12050f89ce84c8983a
pengcheng <peng.cheng@acyclic.ai>
2026-02-28 21:12:00 -0500

    more example

--- core/src/main/scala/com/tribbloids/spike/dotty/VarianceDilemma.scala
+++ core/src/main/scala/com/tribbloids/spike/dotty/VarianceDilemma.scala
@@ -4,7 +4,19 @@ object VarianceDilemma {
 
   trait SS[T]
 
-  trait SS1[+T] extends SS[T]
-  trait SS2[-T] extends SS[T]
+  {
+// these should completely safe, equivalent to TT1 & TT2, yet they fail to typecheck
+//    trait SS1[+T] extends SS[T]
+//    trait SS2[-T] extends SS[T]
+  }
 
+  trait TT1[T] extends SS[T] {
+
+    implicit def co[T1, T2 <: T1]: TT1[T2] <:< TT1[T1]
+  }
+
+  trait TT2[T] extends SS[T] {
+
+    implicit def contra[T1, T2 >: T1]: TT1[T2] <:< TT1[T1]
+  }
 }
--- core/src/test/scala/com/tribbloids/spike/dotty/capture/CaptureCheckingCanBeWiden.scala
+++ core/src/test/scala/com/tribbloids/spike/dotty/capture/CaptureCheckingCanBeWiden.scala
@@ -15,8 +15,8 @@ object CaptureCheckingCanBeWiden {
       val x2: T1^{c1} = c1 // no cast
       val x2_ : T1^ = c1 // ditto, but shorter
       val x3: T1^{c1, c2} = c1 // widening
-      val x1: T1 = c1 //  unsafe downcast, breaks compilation
-      val x4: T1^{c2} = c1 // not a supertype or subtype, breaks compilation
+//      val x1: T1 = c1 //  unsafe downcast, breaks compilation
+//      val x4: T1^{c2} = c1 // not a supertype or subtype, breaks compilation
       ()
     )
   }
